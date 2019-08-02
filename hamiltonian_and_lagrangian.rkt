e484c8e7a9adb6f0d851d1a82be6bc0b0c2675c4
Aditya Chaturvedi <39881830+aditya2912@users.noreply.github.com>
2019-08-03 00:19:20 +0530

    Add files via upload

---
+++ hamiltonian_and_lagrangian.rkt
@@ -0,0 +1,19 @@
+#lang racket
+; Since we have lists as a program in lisp (a procedure is treated as a list), how does one list communicate with another when we call an identifier
+
+(define (is_valid_element? current_element marks_array)
+  (display marks_array)
+  (newline)
+  (cond
+    ((null? marks_array) #t)
+    ((< current_element (car marks_array)) #f)
+    (else (is_valid_element? current_element (cdr marks_array)))))
+
+(define (find_marks marks_array greater_marks)
+  (cond
+    ((null? marks_array) greater_marks)
+    ((is_valid_element? (car marks_array) (cdr marks_array))  (find_marks (cdr marks_array) (cons (car marks_array) greater_marks)))
+    ((not (is_valid_element? (car marks_array) (cdr marks_array))) (find_marks (cdr marks_array) greater_marks))
+    (else (find_marks (cdr marks_array ) greater_marks))))
+
+(find_marks '(16 17 4 3 5 2) '())
