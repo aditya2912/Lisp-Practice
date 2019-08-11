6179ec2842165e926db46e810faf47e791d383f2
Aditya Chaturvedi <39881830+aditya2912@users.noreply.github.com>
2019-08-12 00:27:36 +0530

    Update reading_elm_file.rkt

--- reading_elm_file.rkt
+++ reading_elm_file.rkt
@@ -3,16 +3,33 @@
 ; C:/practice Programs/Elm Projects/src/question_24.elm
 (define file_format "")
 
+(define file_as_list '())
+
 (define types_in_file '())
 
 (define functions '())
 
 (define type_aliases '())
 
-(define (identify_type_aliases)
-  (map (lambda (line) (cond
-                      ((regexp-match #rx"type alias [A-Z]" line) (set! type_aliases (cons line type_aliases)))))
-       file_format))
+(define (find_body file_as_list)
+  (cond
+    ((null? file_as_list) type_aliases)
+    ((regexp-match #rx" }\r" (car file_as_list)) type_aliases)
+    (else (set! type_aliases (append type_aliases (list (car file_as_list)))) (find_body (cdr file_as_list)))))
+
+(define (identify_type_aliases file_as_list)
+  (cond
+    ((null? file_as_list) null)
+    ((regexp-match #rx"type alias [A-Z]" (car file_as_list))
+     ;(display (car file_as_list))
+     (find_body file_as_list)
+     )
+    (else (identify_type_aliases (cdr file_as_list)))))
+
+;(define (identify_type_aliases)
+;  (map (lambda (line) (cond
+;                      ((regexp-match #rx"type alias [A-Z]" line) (set! type_aliases (cons line type_aliases)))))
+;       file_format))
 
 (define (identify_types)
   (map (lambda (line) (cond
@@ -23,15 +40,17 @@
   (map (lambda (line) (cond
                         ((and (regexp-match #rx"[a-z] : " line)
                               (regexp-match #rx" ->" line))
-                          
-                         
-                         (set! functions (cons line functions))))
-         )file_format))
+                         (set! functions (cons line functions)))))
+       file_format))
 
-(let ((file (file->string "C:/practice Programs/Elm Projects/src/question_24.elm")))
+(let ((file (file->string "C:/WebApps/eis/assets/elm/OrgIdentity/Main.elm")))
   (set! file_format file)
-  (set! file_format (string-split file_format "\n"))
-  (identify_types)
-  (identify_type_aliases)
-  (identify_functions)
-  functions)
+  (set! file_as_list (string-split file_format "\n"))
+  ;(identify_types)
+  (identify_type_aliases file_as_list)
+  (display "TYPE ALIAS")
+  type_aliases
+  ;type_aliases
+  ;(identify_functions)
+  ;functions
+  )
