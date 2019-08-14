d4511edf52e8cb6e44785e95c40e657e736ec053
Aditya Chaturvedi <39881830+aditya2912@users.noreply.github.com>
2019-08-14 19:13:57 +0530

    Update reading_elm_file.rkt

--- reading_elm_file.rkt
+++ reading_elm_file.rkt
@@ -9,47 +9,51 @@
 
 (define functions '())
 
-(define type_aliases '())
+(define type_aliases '()) 
+
+(define (find_body_for_type file_as_list)
+  (cond ((null? file_as_list) types_in_file)
+        ((and (regexp-match #rx"\r" (car file_as_list))
+              (regexp-match #rx"\r" (cadr file_as_list)))
+         (display types_in_file)
+         (set! types_in_file (append types_in_file (list (car file_as_list))))
+         (set! types_in_file (append types_in_file (list (cadr file_as_list))))
+         (identify_types file_as_list))
+        (else
+          (set! types_in_file (append types_in_file (list (car file_as_list))))
+          (find_body_for_type (cdr file_as_list)))))
+
+(define (identify_types file_as_list)
+  (cond
+    ((null? file_as_list) null)
+    ((regexp-match #rx"type [A-Z]" (car file_as_list)) (find_body_for_type file_as_list))
+    (else (identify_types (cdr file_as_list)))))
 
 (define (find_body file_as_list)
   (cond
     ((null? file_as_list) type_aliases)
-    ((regexp-match #rx" }\r" (car file_as_list)) type_aliases)
-    (else (set! type_aliases (append type_aliases (list (car file_as_list)))) (find_body (cdr file_as_list)))))
+    ((regexp-match #rx" }\r" (car file_as_list))
+     (set! type_aliases (append type_aliases (list (car file_as_list))))
+     (identify_type_aliases file_as_list))
+    (else
+     (set! type_aliases  (append type_aliases (list (car file_as_list))))
+     (find_body (cdr file_as_list)))))
 
 (define (identify_type_aliases file_as_list)
   (cond
     ((null? file_as_list) null)
     ((regexp-match #rx"type alias [A-Z]" (car file_as_list))
-     ;(display (car file_as_list))
      (find_body file_as_list)
      )
     (else (identify_type_aliases (cdr file_as_list)))))
 
-;(define (identify_type_aliases)
-;  (map (lambda (line) (cond
-;                      ((regexp-match #rx"type alias [A-Z]" line) (set! type_aliases (cons line type_aliases)))))
-;       file_format))
-
-(define (identify_types)
-  (map (lambda (line) (cond
-                      ((regexp-match #rx"type [A-Z]" line) (set! types_in_file (cons line types_in_file)))))
-       file_format))
-
-(define (identify_functions)
-  (map (lambda (line) (cond
-                        ((and (regexp-match #rx"[a-z] : " line)
-                              (regexp-match #rx" ->" line))
-                         (set! functions (cons line functions)))))
-       file_format))
-
 (let ((file (file->string "C:/WebApps/eis/assets/elm/OrgIdentity/Main.elm")))
   (set! file_format file)
   (set! file_as_list (string-split file_format "\n"))
-  ;(identify_types)
-  (identify_type_aliases file_as_list)
-  (display "TYPE ALIAS")
-  type_aliases
+  ;(identify_type_aliases file_as_list)
+  (identify_types file_as_list)
+  types_in_file
+  ;type_aliases
   ;type_aliases
   ;(identify_functions)
   ;functions
